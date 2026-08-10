503c50aa196874b54490e1d54a1fe6fa1360c4bf
Shekinah Cage <scage@catalyte.io>
2026-08-10 15:58:21 -0500

    added database import to schema and fixed tests.

--- src/baked_good/baked_good_schema.py
+++ src/baked_good/baked_good_schema.py
@@ -1,13 +1,13 @@
 from sqlalchemy import Boolean, Column, Float, Integer, String
-# from database import Base
+from database import Base
 
-class BakedGoodSchema():
+class BakedGoodSchema(Base):
 
     __tablename__ = "baked_goods"
     
     id = Column(Integer, primary_key=True) 
-    active: Column(Boolean, default=True)
-    name: Column(String, nullable=False) 
-    description: Column(String, nullable=False)
-    purchasing_cost: Column(Float, nullable=False)
-    retail_price: Column(Float, nullable=False)
+    active = Column(Boolean, default=True)
+    name = Column(String, nullable=False) 
+    description = Column(String, nullable=False)
+    purchasing_cost = Column(Float, nullable=False)
+    retail_price = Column(Float, nullable=False)
