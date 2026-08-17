ef4e534f167b2ff5cddd741be0ca62dc9c04eb11
Shekinah Cage <scage@catalyte.io>
2026-08-17 09:32:58 -0500

    worked on repository for promotion.

--- src/promotion/promotion_repository.py
+++ src/promotion/promotion_repository.py
@@ -1,17 +1,19 @@
-from promotion import promotion_model
-from promotion import promotion_schema
-from src.promotion.promotion_response_model import PromotionResponseModel
-from sqlalchemy.orm import Session
+# from promotion.promotion_model import Promotion
+from promotion.promotion_schema import PromotionSchema
+from promotion.promotion_response_model import PromotionResponseModel
+from sqlalchemy import Session
 from typing import List
+
+from src.promotion.promotion_model import Promotion
+from src.promotion.promotion_schema import PromotionSchema
 
 
 class PromotionRepository():
-    def __init__(self) -> None:
+    def __init__(self, session: Session) -> None:
 
-        self.Promotion: List[PromotionResponseModel] = []
+        self.session = session 
 
-
-    def create_promotion(self, promotion: PromotionResponseModel) ->PromotionResponseModel:
-        
+    def create_promotion(self, promotion: PromotionSchema) ->PromotionSchema:
+        new_promotion= Promotion
         self.promotion.append(promotion)
         return promotion
