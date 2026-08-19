72f09d1f8e114f2765b3165aac0f930dbf0430ea
cjulias-catalyte <cjulias@catalyte.io>
2026-08-19 11:58:08 -0500

    fixed test files

--- tests/employee_tests/test_employee_model.py
+++ tests/employee_tests/test_employee_model.py
@@ -24,24 +24,42 @@ def test_employee_create():
     assert employee.hire_date == date(2023, 1, 1)
 
 
-def test_employee_first_name_invalid():
-    with pytest.raises(ValidationError):
-        Employee(
-            active=True,
-            first_name="John ",
-            last_name="Doe",
-            email="johndoe@deer.com",
-        )
+def test_employee_first_name_whitespace():
+    employee = Employee(
+        active=True,
+        first_name=" John ",
+        last_name="Doe",
+        email="johndoe@deer.com",
+        role="manager",
+        hourly_rate=19.00,
+        hire_date="01/01/2023",
+    )
+    assert employee.active is True
+    assert employee.first_name == "John"
+    assert employee.last_name == "Doe"
+    assert employee.email == "johndoe@deer.com"
+    assert employee.role == EmployeeRole.MANAGER
+    assert employee.hourly_rate == 19.00
+    assert employee.hire_date == date(2023, 1, 1)
 
 
-def test_employee_last_name_invalid():
-    with pytest.raises(ValidationError):
-        Employee(
-            active=True,
-            first_name="John",
-            last_name=" Doe ",
-            email="johndoe@deer.com",
-        )
+def test_employee_last_name_whitespace():
+    employee = Employee(
+        active=True,
+        first_name="John",
+        last_name=" Doe ",
+        email="johndoe@deer.com",
+        role="manager",
+        hourly_rate=19.00,
+        hire_date="01/01/2023",
+    )
+    assert employee.active is True
+    assert employee.first_name == "John"
+    assert employee.last_name == "Doe"
+    assert employee.email == "johndoe@deer.com"
+    assert employee.role == EmployeeRole.MANAGER
+    assert employee.hourly_rate == 19.00
+    assert employee.hire_date == date(2023, 1, 1)
 
 
 def test_employee_first_name_too_long():
