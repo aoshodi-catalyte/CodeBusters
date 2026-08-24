fa6e526386bb1b94e931a71346b4585d60e6f761
Shekinah Cage <scage@catalyte.io>
2026-08-24 13:59:55 -0500

    pylint.

--- src/baked_good/baked_good_repository.py
+++ src/baked_good/baked_good_repository.py
@@ -98,3 +98,4 @@ class BakedGoodRepository:
                 or None if the baked good does not exist.
         """
         return self.session.query(BakedGoodSchema).filter(BakedGoodSchema.id == baked_good_id).first()
+
