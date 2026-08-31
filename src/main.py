0e1308d8b89504cf7174200370a097c22b602960
Christian Julias <cjulias@catalyte.io>
2026-08-31 14:45:59 -0500

    fixing linting

--- src/main.py
+++ src/main.py
@@ -20,9 +20,8 @@ from routers.drink_recipe_router import router as drink_recipe_router
 from routers.employee_router import router as employee_router
 from routers.ingredient_router import router as ingredient_router
 from routers.promotion_router import router as promotion_router
-from routers.vendor_router import router as vendor_router
-from health.health_router import router as health_router
 from routers.secure_login_router import router as secure_login_router
+from routers.vendor_router import router as vendor_router
 
 
 @asynccontextmanager
