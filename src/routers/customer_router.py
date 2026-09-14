92a742eaebdf0630730a5303ebef549055b431c3
cjulias-catalyte <cjulias@catalyte.io>
2026-09-14 14:06:44 -0500

    cod-55-Implement-Deactivate-Records-for-Customer (#81)
    * cod-55-Implement-Deactivate-Records-for-Customer
    
    * fixed functions
    
    * updated so that deactivated database table shows first and last name
    
    * added helper function for router endpoints
    
    * pylinting
    
    * fixing duplicate code for pylint
    
    * removed unsued exception
    
    * fixing test files for customer repository and router

--- src/routers/customer_router.py
+++ src/routers/customer_router.py
@@ -21,9 +21,11 @@ from customer.customer_model import (
     CustomerResponse,
     CustomerUpdate,
 )
-from database import get_db
+from database import get_db, get_audit_db
+from repositories.deactivate_audit_repository import AuditRepository
 from repositories.customer_repository import CustomerRepository
 from security.secure_manager_login import check_role
+from utils.get_acting_user import get_acting_user
 
 
 router = APIRouter()
@@ -65,6 +67,7 @@ def create_customer(
         ) from exc
 
 # pylint: enable=duplicate-code
+
 
 @router.put(
     "/customers/{customer_id}",
@@ -168,7 +171,9 @@ def get_customer(
 )
 def deactivate_customer(
     customer_id: int,
+    token_payload: dict = Depends(check_role(["manager"])),
     db: Session = Depends(get_db),
+    audit_db: Session = Depends(get_audit_db),
 ):
     """
     Deactivate a customer (soft delete) by setting active to False.
@@ -179,11 +184,13 @@ def deactivate_customer(
         HTTPException 404:
             If no customer exists with the provided ID.
     """
+    acting_user = get_acting_user(token_payload, db)
+
     repo = CustomerRepository(db)
+    audit_repo = AuditRepository(audit_db)
 
     try:
-        repo.deactivate_customer(customer_id)
-
+        repo.deactivate_customer(customer_id, acting_user.email, audit_repo)
     except CustomerNotFoundError as exc:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
