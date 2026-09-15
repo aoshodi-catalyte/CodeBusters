e80a6b53a27197de7b30bce9eed14ba8a005f6a7
arenasyncs-lgtm <arenasyncs@gmail.com>
2026-09-15 13:33:07 -0500

    Test Fix

--- src/config.py
+++ src/config.py
@@ -32,7 +32,6 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET_KEY: str
     AUDIT_DATABASE_URL: str
-    JWT_SECRET_KEY: str | None = None
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
